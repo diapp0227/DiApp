f7a8241174be3d8ee7601144807e08f19f5960c0
indianelephant <elephantindian0117@gmail.com>
2024-08-31 17:01:51 +0900

    プロジェクトを作る

---
+++ DiApp/DiApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiApp
+//
+//  Created by garigari0118 on 2024/08/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
