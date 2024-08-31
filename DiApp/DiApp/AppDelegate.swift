a9c04cb3eadc8894a14105ad9cc461774f463de8
diapp0227 <diapp0227@gmail.com>
2024-08-31 18:50:34 +0900

    ログクラス追加

--- DiApp/DiApp/AppDelegate.swift
+++ DiApp/DiApp/AppDelegate.swift
@@ -14,9 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        print("[Print] application didFinishLaunchingWithOptions")
-        debugPrint("[DebugPrint] application didFinishLaunchingWithOptions")
-        NSLog("[NSLog] application didFinishLaunchingWithOptions")
+        Logger.log("application didFinishLaunchingWithOptions")
         
         return true
     }
