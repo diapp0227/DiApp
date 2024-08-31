2b911412adc6f25e4a6e2b6decbb917cc4075f78
indianelephant <elephantindian0117@gmail.com>
2024-09-01 02:23:28 +0900

    時間計算画面にTableViewを追加

---
+++ DiApp/DiApp/Views/TimeCalc/TimeCalcAddElementsCell.swift
@@ -0,0 +1,23 @@
+//
+//  TimeCalcAddElementsCell.swift
+//  DiApp
+//
+//  Created by garigari0118 on 2024/09/01.
+//
+
+import UIKit
+
+class TimeCalcAddElementsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
